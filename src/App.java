3a2d5ed0e913eca581bf681cedc9b7996bc918b1
Gabriel-Paes <paessouzagabriel@gmail.com>
2022-09-24 11:44:28 -0300

    First commit

---
+++ src/App.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);        
+        
+        System.out.print("Digite a primeira nota: ");
+        int nota1 = sc.nextInt();
+
+        System.out.print("Digite a segunda nota: ");
+        int nota2 = sc.nextInt();
+
+        System.out.print("Digite a terceira nota: ");
+        int nota3 = sc.nextInt();
+
+        System.out.print("Digite a quarta nota: ");
+        int nota4 = sc.nextInt();
+        
+        sc.close();
+
+        double media = (nota1+nota2+nota3+nota4)/4;
+
+        System.out.println("A soma dos números informado é: "+media);
+    }
+}
